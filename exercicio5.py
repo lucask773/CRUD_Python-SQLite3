95aa2cf0d08b497fe4e0ff3b39f555978a8a6fef
Kevin <65177404+lucask773@users.noreply.github.com>
2025-01-27 10:57:34 -0300

    Delete exercicio5.py

--- exercicio5.py
+++
@@ -1,9 +0,0 @@
-velocidade = 61    # velocidade atual do carro
-local_carro = 100  # local em que o carro está na estrada
-
-RADAR_1 = 60  # velocidade máxima do radar 1
-LOCAL_1 = 100  # local onde o radar 1 está
-RADAR_RANGE = 1  # A distância onde o radar pega
-
-if velocidade > RADAR_1:
-    print('VOCÊ FOI MULTADO')
